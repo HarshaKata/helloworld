c4b26e3b2e4bd8750ad218c4fcd6e64db8b97413
Harsha Kata <harshakata@Harshas-MBP.attlocal.net>
2024-09-04 22:27:22 -0700

    added assignment summary document

--- combine.js
+++ combine.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function getCombinedMessage() {
     try {
-        const helloResponse = await axios.get('http://localhost:3002/hello'); /* alternatively 'http://127.0.0.1:59669/hello' can also be used - this url is returned when 'minikube service hello' is executed */
+        const helloResponse = await axios.get('http://localhost:3002/hello'); /* alternatively 'http://127.0.0.1:53665/hello' can also be used - this url is returned when 'minikube service hello' is executed */
         const worldResponse = await axios.get('http://localhost:3003/world'); /* alternatively 'http://127.0.0.1:59692/world' can also be used - this url is returned when 'minikube service world' is executed */
         console.log(`${helloResponse.data} ${worldResponse.data}`);
     } catch (error) {
